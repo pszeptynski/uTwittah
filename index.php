5ef7beba89e171ab36c76d8a9cc0013b727a1cb8
Piotr Szeptynski <p.szeptynski@gmail.com>
2016-09-04 20:50:34 +0200

    Create index.php with two forms for log in and sign up. Create html header and footer.

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+require_once('src/init.php');
+var_dump($_SESSION);
+
+
+
+//$_SESSION['logged_user_id'] = $id
+// sprawdz czy user zalogowany, jesli nie wyswietl strone z rejestracja i logowaniem
+
+if (isset($_SESSION['logged_user_id'])) {
+// jesli zalogowany to wczytaj main.php
+//header("location: main.php");
+} else {
+    
+}
+
+
+
+
+
+include('html/header.html');
+?>
+
+<h1>Welcome to uTwittah!</h1>
+<p>
+    Connect with your friends — and other fascinating people.
+    Get in-the-moment updates on the things that interest you.
+    And watch events unfold, in real time, from every angle.
+
+</p>
+
+<div id="login-form">
+    <fieldset>
+        <legend>Login to continue!</legend>
+        <form action="#" method="post">
+            <input type="email" name="email" placeholder="Your e-mail">
+            <br>
+            <input type="password" name="password" placeholder="Password">
+            <br>
+            <label>
+                <input type="checkbox" value="1" name="remember_me">
+                <span>Remember me</span>
+            </label>
+            <br>
+            <input type="submit" value="Log in">
+        </form>
+    </fieldset>
+</div>
+
+<div id="signup-form">
+    <fieldset>
+        <legend>New user? Sign up!</legend>
+        <form action="#" method="post">
+            <input type="text" name="username" placeholder="Username">
+            <br>
+            <input type="email" name="email" placeholder="E-mail">
+            <br>
+            <input type="password" name="password" placeholder="Password">
+            <br>
+            <input type="submit" value="Log in">
+        </form>
+    </fieldset>
+</div>
+
+
+
+
+
+
+
+<?php
+include('html/footer.html');
+?>
